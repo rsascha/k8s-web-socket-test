5bca5cb3774279d764842bffb1682040d68f7f66
Sascha Rose <sascha.rose@gmail.com>
2020-11-01 22:58:14 +0100

    Backup: subscriber and socket service running in k8s with 1 pod

--- subscriber/src/app/app.module.ts
+++ subscriber/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
 
 const config: SocketIoConfig = {
-    url: 'http://localhost:3000',
+    url: 'http://socket-server.127.0.0.1.nip.io',
     options: {},
 };
 
